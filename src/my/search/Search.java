49e82a44caa22ca1a987ae8f48d1184e7f8dceec
michal <michal.dio@seznam.cz>
2022-12-10 17:10:00 +0100

    refactoring

--- src/my/search/Search.java
+++ src/my/search/Search.java
@@ -7,7 +7,7 @@ public class Search {
     public static void main(String[] args) {
 
         Search srch = new Search();
-        int[] a = srch.fillArrayRandomly(12);
+        int[] a = srch.fillArrayRandomly(13);
         int toSearch = a[3];
 
         System.out.println(Arrays.toString(a));
